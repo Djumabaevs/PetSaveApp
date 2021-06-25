7dd22893e00cb21a5b802e901b1dca06e368ad5a
Djumabaevs <djumabaevb@gmail.com>
2021-06-25 10:28:36 +0600

    structure of projects - packages

--- app/src/main/java/com/bignerdranch/android/petsaveapp/common/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/petsaveapp/common/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.bignerdranch.android.petsaveapp
+package com.bignerdranch.android.petsaveapp.common
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bignerdranch.android.petsaveapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
