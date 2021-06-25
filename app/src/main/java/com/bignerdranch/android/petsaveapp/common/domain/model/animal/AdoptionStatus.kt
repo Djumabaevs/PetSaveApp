d46bd4490fb92fa43d827fe65db236ba889b81f1
Djumabaevs <djumabaevb@gmail.com>
2021-06-25 10:54:17 +0600

    add data classes and enums

---
+++ app/src/main/java/com/bignerdranch/android/petsaveapp/common/domain/model/animal/AdoptionStatus.kt
@@ -0,0 +1,8 @@
+package com.bignerdranch.android.petsaveapp.common.domain.model.animal
+
+enum class AdoptionStatus {
+    UNKNOWN,
+    ADOPTABLE,
+    ADOPTED,
+    FOUND
+}
---
+++ app/src/main/java/com/bignerdranch/android/petsaveapp/common/domain/model/animal/details/Age.kt
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.petsaveapp.common.domain.model.animal.details
+
+enum class Age {
+    UNKNOWN,
+    BABY,
+    YOUNG,
+    ADULT,
+    SENIOR
+}
