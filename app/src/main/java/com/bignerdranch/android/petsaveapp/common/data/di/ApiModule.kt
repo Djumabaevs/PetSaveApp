54c40c766482097f0c8c6100d0ee8921a5acfb7b
Djumabaevs <djumabaevb@gmail.com>
2021-06-25 12:50:34 +0600

    add module and installin to api module class

---
+++ app/src/main/java/com/bignerdranch/android/petsaveapp/common/data/di/ApiModule.kt
@@ -0,0 +1,57 @@
+package com.bignerdranch.android.petsaveapp.common.data.di
+
+import com.bignerdranch.android.petsaveapp.common.data.api.ApiConstants
+import com.bignerdranch.android.petsaveapp.common.data.api.PetFinderApi
+import com.bignerdranch.android.petsaveapp.common.data.api.interceptors.AuthenticationInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+
+    @Provides
+    @Singleton
+    fun provideApi(builder: Retrofit.Builder): PetFinderApi {
+        return builder
+            .build()
+            .create(PetFinderApi::class.java)
+    }
+
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit.Builder {
+        return Retrofit.Builder()
+            .baseUrl(ApiConstants.BASE_ENDPOINT)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create())
+    }
+
+    @Provides
+    fun provideOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+        networkStatusInterceptor: NetworkStatusInterceptor,
+        authenticationInterceptor: AuthenticationInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(networkStatusInterceptor)
+            .addInterceptor(authenticationInterceptor)
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    fun provideHttpLoggingInterceptor(loggingInterceptor: LoggingInterceptor): HttpLoggingInterceptor {
+        val interceptor = HttpLoggingInterceptor(loggingInterceptor)
+
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        return interceptor
+    }
+}
