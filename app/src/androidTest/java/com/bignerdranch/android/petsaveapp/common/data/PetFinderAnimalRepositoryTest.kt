27b07e1ac9aaa3d746c8883f73790493206d2252
Djumabaevs <djumabaevb@gmail.com>
2021-06-26 13:29:47 +0600

    hilt test runner

---
+++ app/src/androidTest/java/com/bignerdranch/android/petsaveapp/common/data/PetFinderAnimalRepositoryTest.kt
@@ -0,0 +1,30 @@
+package com.bignerdranch.android.petsaveapp.common.data
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class PetFinderAnimalRepositoryTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun getAnimals() {
+    }
+
+    @Test
+    fun requestMoreAnimals() {
+    }
+
+    @Test
+    fun storeAnimals() {
+    }
+}
