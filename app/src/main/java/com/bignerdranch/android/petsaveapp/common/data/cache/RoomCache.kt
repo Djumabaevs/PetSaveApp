cda5cbc419bda410feadfce20f79e43666f83719
Djumabaevs <djumabaevb@gmail.com>
2021-06-26 11:32:56 +0600

    implementing methods in room cache

--- app/src/main/java/com/bignerdranch/android/petsaveapp/common/data/cache/RoomCache.kt
+++ app/src/main/java/com/bignerdranch/android/petsaveapp/common/data/cache/RoomCache.kt
@@ -1,15 +1,27 @@
 package com.bignerdranch.android.petsaveapp.common.data.cache
 
+import com.bignerdranch.android.petsaveapp.common.data.cache.daos.AnimalsDao
 import com.bignerdranch.android.petsaveapp.common.data.cache.daos.OrganizationsDao
+import com.bignerdranch.android.petsaveapp.common.data.cache.model.cachedanimal.CachedAnimalAggregate
 import com.bignerdranch.android.petsaveapp.common.data.cache.model.cachedorganization.CachedOrganization
+import io.reactivex.Flowable
 import javax.inject.Inject
 
 
 class RoomCache @Inject constructor(
+    private val animalsDao: AnimalsDao,
     private val organizationsDao: OrganizationsDao
 ) : Cache {
 
     override suspend fun storeOrganizations(organizations: List<CachedOrganization>) {
         organizationsDao.insert(organizations)
+    }
+
+    override fun getNearbyAnimals(): Flowable<List<CachedAnimalAggregate>> {
+        return animalsDao.getAllAnimals()
+    }
+
+    override suspend fun storeNearbyAnimals(animals: List<CachedAnimalAggregate>) {
+       animalsDao.insertAnimalsWithDetails(animals)
     }
 }
