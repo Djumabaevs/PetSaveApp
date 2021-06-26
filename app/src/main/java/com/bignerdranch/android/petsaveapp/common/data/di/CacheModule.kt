9ce4f62357d79969321f606bf2d770900808f135
Djumabaevs <djumabaevb@gmail.com>
2021-06-26 12:05:33 +0600

    add cache module in companion

--- app/src/main/java/com/bignerdranch/android/petsaveapp/common/data/di/CacheModule.kt
+++ app/src/main/java/com/bignerdranch/android/petsaveapp/common/data/di/CacheModule.kt
@@ -1,10 +1,14 @@
 package com.bignerdranch.android.petsaveapp.common.data.di
 
+import android.content.Context
+import androidx.room.Room
 import com.bignerdranch.android.petsaveapp.common.data.cache.PetSaveDatabase
+import com.bignerdranch.android.petsaveapp.common.data.cache.daos.AnimalsDao
 import com.bignerdranch.android.petsaveapp.common.data.cache.daos.OrganizationsDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -16,6 +20,21 @@ abstract class CacheModule {
 
         @Provides
         @Singleton
+        fun provideDatabase(
+            @ApplicationContext context: Context
+        ): PetSaveDatabase {
+            return Room.databaseBuilder(
+                context,
+                PetSaveDatabase::class.java,
+                "petsave.db"
+            )
+                .build()
+        }
+
+        @Provides
+        fun provideAnimalsDao(
+            petSaveDatabase: PetSaveDatabase
+        ): AnimalsDao = petSaveDatabase.animalsDao()
 
 
         @Provides
