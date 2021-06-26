61f6becb995b6242109b2ab5744e741c39066d43
Djumabaevs <djumabaevb@gmail.com>
2021-06-26 10:41:08 +0600

    suspend fun for getting cached data in insert fun

--- app/src/main/java/com/bignerdranch/android/petsaveapp/common/data/cache/daos/AnimalsDao.kt
+++ app/src/main/java/com/bignerdranch/android/petsaveapp/common/data/cache/daos/AnimalsDao.kt
@@ -1,9 +1,7 @@
 package com.bignerdranch.android.petsaveapp.common.data.cache.daos
 
-import androidx.room.Dao
-import androidx.room.Query
-import androidx.room.Transaction
-import com.bignerdranch.android.petsaveapp.common.data.cache.model.cachedanimal.CachedAnimalAggregate
+import androidx.room.*
+import com.bignerdranch.android.petsaveapp.common.data.cache.model.cachedanimal.*
 import io.reactivex.Flowable
 
 @Dao
@@ -12,5 +10,13 @@ abstract class AnimalsDao {
     @Transaction
     @Query("SELECT * FROM animals")
     abstract fun getAllAnimals(): Flowable<List<CachedAnimalAggregate>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    abstract suspend fun insertAnimalAggregate(
+        animal: CachedAnimalWithDetails,
+        photos: List<CachedPhoto>,
+        video: List<CachedVideo>,
+        tags: List<CachedTag>
+    )
 
 }
