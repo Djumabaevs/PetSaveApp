9dd5c0f788c43170558cbe61be5c9848e231657b
Djumabaevs <djumabaevb@gmail.com>
2021-07-13 14:57:43 +0600

    add sharing component to di of sharing module

---
+++ sharing/src/main/java/com/djumabaevs/sharing/di/SharingComponent.kt
@@ -0,0 +1,21 @@
+package com.djumabaevs.sharing.di
+
+import android.content.Context
+import dagger.BindsInstance
+import dagger.Component
+
+@Component(
+    dependencies = [SharingModuleDependencies::class],
+    modules = [SharingModule::class]
+)
+interface SharingComponent {
+
+    fun inject(fragment: SharingFragment)
+
+    @Component.Builder
+    interface Builder {
+        fun context(@BindsInstance context: Context): Builder
+        fun moduleDependencies(sharingModuleDependencies: SharingModuleDependencies): Builder
+        fun build(): SharingComponent
+    }
+}
