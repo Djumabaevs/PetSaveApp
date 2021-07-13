7ec2b4e9690f82ceda9d36802b2be38962adc401
Djumabaevs <djumabaevb@gmail.com>
2021-07-13 15:05:11 +0600

    UiAnimalToShareMapper

--- sharing/src/main/java/com/djumabaevs/sharing/presentation/SharingViewState.kt
+++ sharing/src/main/java/com/djumabaevs/sharing/presentation/SharingViewState.kt
@@ -1,6 +1,6 @@
 package com.djumabaevs.sharing.presentation
 
-import com.raywenderlich.android.petsave.sharing.presentation.model.UIAnimalToShare
+
 
 data class SharingViewState(
     val animalToShare: UIAnimalToShare = UIAnimalToShare(image = "", defaultMessage = "")
---
+++ sharing/src/main/java/com/djumabaevs/sharing/presentation/model/UIAnimalToShare.kt
@@ -0,0 +1,3 @@
+package com.djumabaevs.sharing.presentation.model
+
+data class UIAnimalToShare(val image: String, val defaultMessage: String)
