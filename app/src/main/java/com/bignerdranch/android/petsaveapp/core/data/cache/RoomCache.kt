af0048f1b3ad7c9439b33655ab7f045f84c90f65
Djumabaevs <djumabaevb@gmail.com>
2021-08-01 13:28:32 +0600

    touch id bug security

--- app/src/main/java/com/bignerdranch/android/petsaveapp/core/data/cache/RoomCache.kt
+++ app/src/main/java/com/bignerdranch/android/petsaveapp/core/data/cache/RoomCache.kt
@@ -24,6 +24,7 @@ class RoomCache @Inject constructor(
     animalsDao.insertAnimalsWithDetails(animals)
   }
 
+
   override suspend fun getAllTypes(): List<String> {
     return animalsDao.getAllTypes()
   }
