a92a37d16f392cffc2f04537b6184b8d81c88489
Djumabaevs <djumabaevb@gmail.com>
2021-08-14 11:45:14 +0600

    init block

--- app/src/main/java/com/bignerdranch/android/petsaveapp/core/presentation/ProgressButton.kt
+++ app/src/main/java/com/bignerdranch/android/petsaveapp/core/presentation/ProgressButton.kt
@@ -2,11 +2,14 @@ package com.bignerdranch.android.petsaveapp.core.presentation
 
 import android.animation.ValueAnimator
 import android.content.Context
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.RectF
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.View
 import com.bignerdranch.android.petsaveapp.R
+import com.bignerdranch.android.petsaveapp.core.utils.dpToPx
 
 class ProgressButton @JvmOverloads constructor(
     context: Context,
@@ -47,4 +50,28 @@ class ProgressButton @JvmOverloads constructor(
 
     private var rotationAnimator: ValueAnimator? = null
     private var drawTick = false
+
+    init {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ProgressButton, defStyleAttr, defStyleRes)
+
+        buttonText = typedArray.getString(R.styleable.ProgressButton_progressButton_text) ?: ""
+
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(android.R.attr.colorPrimary, typedValue, true)
+        val defaultBackgroundColor = typedValue.data
+
+        val defaultTextColor = Color.WHITE
+        val defaultProgressColor = Color.WHITE
+
+        val backgroundColor = typedArray.getColor(R.styleable.ProgressButton_progressButton_backgroundColor, defaultBackgroundColor)
+        backgroundPaint.color = backgroundColor
+
+        val textColor = typedArray.getColor(R.styleable.ProgressButton_progressButton_textColor, defaultTextColor)
+        textPaint.color = textColor
+
+        val progressColor = typedArray.getColor(R.styleable.ProgressButton_progressButton_progressColor, defaultProgressColor)
+        progressPaint.color = progressColor
+        typedArray.recycle()
+    }
+
 }
