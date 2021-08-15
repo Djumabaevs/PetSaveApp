2005c17ab168f495d03570da42ab12c0ecc655df
Djumabaevs <djumabaevb@gmail.com>
2021-08-15 11:24:58 +0600

    handle event

--- app/src/main/java/com/bignerdranch/android/petsaveapp/details/presentation/AnimalDetailsViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/petsaveapp/details/presentation/AnimalDetailsViewModel.kt
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.petsaveapp.details.presentation
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.bignerdranch.android.petsaveapp.core.presentation.model.mappers.UiAnimalDetailsMapper
 import com.bignerdranch.android.petsaveapp.details.domain.usecases.AnimalDetails
@@ -11,4 +13,16 @@ class AnimalDetailsViewModel @ViewModelInject constructor(
     private val animalDetails: AnimalDetails,
     private val compositeDisposable: CompositeDisposable
 ): ViewModel() {
+
+    private val _state = MutableLiveData<AnimalDetailsViewState>()
+    val state: LiveData<AnimalDetailsViewState> get() =_state
+
+    init {
+        _state.value = AnimalDetailsViewState.Loading
+    }
+
+    fun handleEvent(event: AnimalDetailsEvent) {
+        is AnimalDetailsEvent.LoadAnimalDetails -> subs
+        is AnimalDetailsEvent.AdoptAnimal -> ado
+    }
 }
