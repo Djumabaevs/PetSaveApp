971c14cae667109ed9d1b1a7aa36501b72417650
Djumabaevs <djumabaevb@gmail.com>
2021-08-15 11:33:53 +0600

    adopt animal and clear and failure funs

--- app/src/main/java/com/bignerdranch/android/petsaveapp/details/presentation/AnimalDetailsViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/petsaveapp/details/presentation/AnimalDetailsViewModel.kt
@@ -4,8 +4,10 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.bignerdranch.android.petsaveapp.core.domain.model.animal.AnimalWithDetails
 import com.bignerdranch.android.petsaveapp.core.presentation.model.mappers.UiAnimalDetailsMapper
 import com.bignerdranch.android.petsaveapp.details.domain.usecases.AnimalDetails
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
@@ -28,7 +30,7 @@ class AnimalDetailsViewModel @ViewModelInject constructor(
     fun handleEvent(event: AnimalDetailsEvent) {
       when(event) {
           is AnimalDetailsEvent.LoadAnimalDetails -> subscribeToAnimalDetails(event.animalId)
-          is AnimalDetailsEvent.AdoptAnimal -> ado
+          is AnimalDetailsEvent.AdoptAnimal -> adoptAnimal()
       }
     }
 
@@ -41,5 +43,30 @@ class AnimalDetailsViewModel @ViewModelInject constructor(
                 { onAnimalDetails(it) },
                 { onFailure(it) }
             ).addTo(compositeDisposable)
+    }
+
+    private fun onAnimalDetails(animal: AnimalWithDetails) {
+        val animalDetails = uiAnimalDetailsMapper.mapToView(animal)
+        _state.value = AnimalDetailsViewState.AnimalDetails(animalDetails)
+    }
+
+    private fun adoptAnimal() {
+        compositeDisposable.add(
+            Observable.timer(2L, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    _state.value = (_state.value as AnimalDetailsViewState.AnimalDetails?)?.copy(adopted = true)
+                }
+        )
+    }
+
+    private fun onFailure(failure: Throwable) {
+        _state.value = AnimalDetailsViewState.Failure
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
     }
 }
