569149542955ca978883031e3cf093b69ccc0727
Djumabaevs <djumabaevb@gmail.com>
2021-08-15 12:16:37 +0600

    dynamic animation objects by lazy in details fragment

--- app/src/main/java/com/bignerdranch/android/petsaveapp/details/presentation/AnimalDetailsFragment.kt
+++ app/src/main/java/com/bignerdranch/android/petsaveapp/details/presentation/AnimalDetailsFragment.kt
@@ -1,4 +1,64 @@
 package com.bignerdranch.android.petsaveapp.details.presentation
 
-class AnimalDetailsFragment {
+import androidx.dynamicanimation.animation.DynamicAnimation
+import androidx.dynamicanimation.animation.FlingAnimation
+import androidx.dynamicanimation.animation.SpringAnimation
+import androidx.dynamicanimation.animation.SpringForce
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.bignerdranch.android.petsaveapp.databinding.FragmentDetailsBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AnimalDetailsFragment: Fragment() {
+
+    companion object{
+        const val ANIMAL_ID = "id"
+    }
+
+    private val FLING_SCALE = 1f
+
+    private val binding get() = _binding!!
+    private var _binding: FragmentDetailsBinding? = null
+
+    private val viewModel: AnimalDetailsViewModel by viewModels()
+
+    private var animalId: Long? = null
+
+    private val springForce: SpringForce by lazy {
+        SpringForce().apply {
+            dampingRatio = SpringForce.DAMPING_RATIO_HIGH_BOUNCY
+            stiffness = SpringForce.STIFFNESS_VERY_LOW
+        }
+    }
+
+    private val callScaleXSpringAnimation: SpringAnimation by lazy {
+        SpringAnimation(binding.call, DynamicAnimation.SCALE_X).apply {
+            spring = springForce
+        }
+    }
+
+    private val callScaleYSpringAnimation: SpringAnimation by lazy {
+        SpringAnimation(binding.call, DynamicAnimation.SCALE_Y).apply {
+            spring = springForce
+        }
+    }
+
+    private val FLING_FRICTION = 2f
+
+    private val callFlingXAnimation: FlingAnimation by lazy {
+        FlingAnimation(binding.call, DynamicAnimation.X).apply {
+            friction = FLING_FRICTION
+            setMinValue(0f)
+            setMaxValue(binding.root.width.toFloat() - binding.call.width.toFloat())
+        }
+    }
+
+    private val callFlingYAnimation: FlingAnimation by lazy {
+        FlingAnimation(binding.call, DynamicAnimation.Y).apply {
+            friction = FLING_FRICTION
+            setMinValue(0f)
+            setMaxValue(binding.root.height.toFloat() - binding.call.width.toFloat())
+        }
+    }
 }
