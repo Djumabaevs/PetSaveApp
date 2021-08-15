e1992d7af64710065c58bc4d230d07c4aa1734b8
Djumabaevs <djumabaevb@gmail.com>
2021-08-15 10:43:00 +0600

    sealed class animal details event

---
+++ app/src/main/java/com/bignerdranch/android/petsaveapp/details/presentation/AnimalDetailsEvent.kt
@@ -0,0 +1,7 @@
+package com.bignerdranch.android.petsaveapp.details.presentation
+
+sealed class AnimalDetailsEvent {
+
+    data class LoadAnimalDetails(val animalId: Long): AnimalDetailsEvent()
+    object AdoptAnimal: AnimalDetailsEvent()
+}
