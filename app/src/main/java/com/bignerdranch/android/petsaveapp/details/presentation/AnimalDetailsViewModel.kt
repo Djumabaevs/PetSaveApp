7b4d2f0dc25c0f7e460c2e5bceb8b2b1edad92e4
Djumabaevs <djumabaevb@gmail.com>
2021-08-15 10:55:05 +0600

    view model for animal with details inject

--- app/src/main/java/com/bignerdranch/android/petsaveapp/details/presentation/AnimalDetailsViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/petsaveapp/details/presentation/AnimalDetailsViewModel.kt
@@ -1,4 +1,14 @@
 package com.bignerdranch.android.petsaveapp.details.presentation
 
-class AnimalDetailsViewModel {
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+import com.bignerdranch.android.petsaveapp.core.presentation.model.mappers.UiAnimalDetailsMapper
+import com.bignerdranch.android.petsaveapp.details.domain.usecases.AnimalDetails
+import io.reactivex.disposables.CompositeDisposable
+
+class AnimalDetailsViewModel @ViewModelInject constructor(
+    private val uiAnimalDetailsMapper: UiAnimalDetailsMapper,
+    private val animalDetails: AnimalDetails,
+    private val compositeDisposable: CompositeDisposable
+): ViewModel() {
 }
